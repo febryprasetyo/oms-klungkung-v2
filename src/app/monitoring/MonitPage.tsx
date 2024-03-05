b416ffdc5aaabbdb38876ae09d6934155cc4f727
febryprasetyo <febryprasetyo23@gmail.com>
2024-03-05 10:23:45 +0700

    feat: ✨ Set up firestore

---
+++ src/app/monitoring/MonitPage.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { onSnapshot, collection, getDoc } from 'firebase/firestore';
+import db from '@/firebase/config';
+import MonitoringCard from '@/component/MonitoringCard';
+import MonitoringCardBod from '@/component/MonitoringCardBod';
+
+const initSpeedometer = {
+  bod: 0,
+  cod: 0,
+  ct: 0,
+  depeth: 0,
+  do_: 0,
+  id: '',
+  n: 0,
+  no2: 0,
+  no3_3: 0,
+  orp: 0,
+  ph: 0,
+  temperature: 0,
+  time: '',
+  tss: 0,
+  tur: 0,
+  uuid: '',
+};
+
+const MonitPage = () => {
+  const id = '240305005225029';
+  const [dataMonitoring, setDataMonitoring] = useState(initSpeedometer);
+  const stationCollectionRef = collection(db, `watermonitoring`);
+
+  useEffect(() => {
+    const unsub = onSnapshot(stationCollectionRef, (snapshot) => {
+      const data: any[] = [];
+      snapshot.docs.forEach((doc) => {
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      const filterById = data.filter((val) => val.uuid == id);
+      if (filterById.length > 0) {
+        setDataMonitoring(filterById[0]);
+      }
+    });
+    return () => unsub();
+  }, [id, stationCollectionRef]);
+
+  return (
+    <main className={`px-5  transition-all sm:px-10 `}>
+      <section>
+        <div className='grid grid-cols-1 gap-4 py-7 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-6'>
+          <MonitoringCard
+            title='Temperature'
+            value={+dataMonitoring.temperature.toFixed(2)}
+            unit={`\u00B0C`}
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='DO'
+            value={+dataMonitoring.do_.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='Turbidity'
+            value={+dataMonitoring.tur.toFixed(2)}
+            unit='NTU'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='TDS'
+            value={+dataMonitoring.ct.toFixed(2)}
+            unit='ppm'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='pH'
+            value={+dataMonitoring.ph.toFixed(2)}
+            unit='pH'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='ORP'
+            value={+dataMonitoring.orp.toFixed(2)}
+            unit='Mv'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCardBod
+            title='BOD'
+            value={+dataMonitoring.bod.toFixed(2)}
+            valuecod={+dataMonitoring.cod.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='COD'
+            value={+dataMonitoring.cod.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='TSS'
+            value={+dataMonitoring.tss.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='Amonia'
+            value={+dataMonitoring.n.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='Nitrat'
+            value={+dataMonitoring.no3_3.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+
+          <MonitoringCard
+            title='Nitrit'
+            value={+dataMonitoring.no2.toFixed(2)}
+            unit='mg/L'
+            time={dataMonitoring.time}
+          />
+          <MonitoringCard
+            title='Kedalaman'
+            value={+dataMonitoring.depeth.toFixed(2)}
+            unit='M'
+            time={dataMonitoring.time}
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default MonitPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
-import Monitorng from './monitoring/page';
 import Navbar from '@/component/Navbar';
+import MonitPage from '@/app/monitoring/MonitPage';
 export default function Home() {
   return (
     <main className='bg-blue-gray-400 min-h-screen pb-4'>
       <Navbar />
-      <Monitorng />
+      <MonitPage />
     </main>
   );
 }
